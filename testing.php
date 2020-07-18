71c48b7bd159998de361effd28a7423c64383565
MehvixDesktop <mehvix@protonmail.com>
2020-07-17 23:40:32 -0500

    chagned name

--- testing.php
+++ testing.php
@@ -1,5 +1,15 @@
 <?php
-    include_once 'includes/dbh.php'
+
+$dbServername   = "localhost";
+$dbUser         = "root";   // default for xampp
+$dbPass         = "";   // default for xampp
+$dbName         = "chats";
+
+$conn = mysqli_connect($dbServername, $dbUser, $dbPass, $dbName);
+
+if ($conn->connect_error or $conn === false) {
+    die("Database Connection failed: " . $conn->connect_error);
+}
 ?>
 
 
@@ -19,7 +29,6 @@
 </form>
 
 <?php
-
     $sql = "SELECT * FROM chat1;";
     $result = mysqli_query($conn, $sql);
     $resultCheck = mysqli_num_rows($result);
