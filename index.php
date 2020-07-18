e0505a8a5e4e7a5859205e06fb15744b3fa35c56
emly <emily2004.yao@gmail.com>
2020-07-18 02:10:39 -0500

    front-end v1.0

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8" /> 
+    <meta name="viewport" content= 
+        "width=device-width, initial-scale=1.0" /> 
+</head>
+
+<div class="header">
+    <h1 class="header">Chadburn</h1>
+    <button class="column"> Create a new chat ID </button>
+    <button class="column"> Open existing chat ID </button>
+    <br><br><br><br><br><br><br><br>
+    <h4 class="header">This is our slogan.</h4>
+</div>
+
+<div class="explanation-left">
+    <h3 class="explanation-left">This is our first point.</h3>
+    <p class="explanation-left">This is our reasoning.</p>
+</div>
+
+<div class="explanation-right">
+    <h3 class="explanation-right">This is our second point.</h3>
+    <p class="explanation-right">This is our reasoning.</p>
+</div>
+
+<div class="explanation-left">
+    <h3 class="explanation-left">This is our third point.</h3>
+    <p class="explanation-left">This is our reasoning.</p>
+</div>
+
+</html>
