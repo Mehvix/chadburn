509d891c5c10dd49e8b5863353f9656f29a451c2
MehvixDesktop <mehvix@protonmail.com>
2020-07-18 16:35:49 -0500

    changed to chars instead, moved static stuff to /static

--- create_chat.php
+++ create_chat.php
@@ -1,38 +1,35 @@
 <?php
 include_once("consts.php");
 
-$new_phrase = $_POST['new-phrase'];
+$new_phrase = ($_POST['new-phrase']);
+
 if (!$new_phrase) {
-    header('Location: http://localhost/chadburn/');  // todo update
+    header('Location: http://' . HOST . '/chadburn/');  // todo update
     exit();
 }
-//    $_SESSION['PHRASE']=$new_phrase;
+
 try {
-    // Creating a connection
-    $connect = new PDO('mysql:host='.HOST.';dbname='.$_SESSION['PHRASE'], USER, PASSWORD);
+    $connect = new PDO('mysql:host=' . HOST, USER, PASSWORD);
 
-    $connect->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );//Error Handling
+    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//Error Handling
 
-
-    // Creating a database named newDB
     $sql = "CREATE DATABASE " . $new_phrase;
-    if ($connect->exec($sql) === TRUE) {
-        echo "Successfully created the chat" . $new_phrase;
-    }
+    $connect->exec($sql);
 
-    $connect->exec('USE '.$new_phrase);
+    $connect->exec('USE ' . $new_phrase);
 
     $sql = 'CREATE TABLE `messages` (
              `id` int(11) primary key not null AUTO_INCREMENT,
              `message` varchar(2048) not null,
-             `author` varchar(40) not null,
+             `author` varchar(128) not null,
              `date` timestamp not null default current_timestamp
             )';
     $connect->exec($sql);
 
 
 } catch (PDOException $e) {
-    echo $e;
+    echo $e->getMessage();
+    die(); // prevent forwarding
 }
 ?>
 
@@ -46,6 +43,7 @@ try {
     function submitForm() {
         document.getElementById('forward_form').submit();
     }
+
     //Call the function submitForm() as soon as the page has loaded.
     window.onload = submitForm;
 </script>
--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <html>
 <head>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="static/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="static/script.js"></script>
     <meta charset="UTF-8"/>
     <meta name="viewport" content=
     "width=device-width, initial-scale=1.0"/>
 </head>
-
+<body onload="getRandomChars()">
 <div class="header" style="height: 300px">
     <h1 class="header">Chadburn</h1>
 
@@ -20,7 +22,7 @@
 
         <div class="flex-child" style="background: #f39292">
             <form id="new" action="create_chat.php" method="POST">
-                <input class="menu" name="new-phrase"" placeholder="Enter new phrase">
+                <input class="menu" id="addWords" name="new-phrase" placeholder="Enter new phrase" value="Generating...">
                 <button class="menu hoverer" type="submit" style="background: #f39292">=>
                 </button>
             </form>
@@ -28,15 +30,15 @@
     </div>
 
 
-    <!--    <div class="b-left" class="column">-->
-    <!--        <button class="hoverer menu">Create a new chat ID</button>-->
-    <!--        <input name="existing-phrase" id="existing-phrase" placeholder="Enter existing phrase">-->
-    <!--    </div>-->
-    <!---->
-    <!--    <div class="b-right" class="column">-->
-    <!--        <button class="hoverer menu">Open existing chat ID</button>-->
-    <!--        <input name="new-phrase" id="new-phrase" placeholder="Enter new phrase">-->
-    <!--    </div>-->
+   <!--     <div class="b-left" class="column">
+            <button class="hoverer menu">Create a new chat ID</button>
+            <input name="existing-phrase" id="existing-phrase" placeholder="Enter existing phrase">
+        </div>
+
+        <div class="b-right" class="column">
+            <button class="hoverer menu">Open existing chat ID</button>
+            <input name="new-phrase" id="new-phrase" placeholder="Enter new phrase">
+        </div>-->
 </div>
 
 <div class="explanation-left">
@@ -56,5 +58,5 @@
     <p class="explanation-left">We have all of our code posted publicly so that you can verify it's legitimacy and host
         your own instance.</p>
 </div>
-
+</body>
 </html>
