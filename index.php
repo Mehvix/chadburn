fdad2f1e87a6424ab3b93d71de86f939bbdf5a4b
MehvixDesktop <mehvix@protonmail.com>
2020-07-17 23:06:13 -0500

    updated

--- index.php
+++ index.php
@@ -10,9 +10,25 @@
 
 <body>
 
+<form action="sendmessage.php" method="POST">
+    <input type="text" name="author" placeholder="Alias">
+    <br>
+    <input type="text" name="content" placeholder="Message">
+    <br>
+    <button type="submit" name="submit">Send Message</button>
+</form>
 
 <?php
-    $sql = "SELECT * FROM chats;"
+
+    $sql = "SELECT * FROM chat1;";
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
+
+    if ($resultCheck > 0){
+        while($row = mysqli_fetch_assoc($result)){
+            echo $row['message'] . "<br>";
+        }
+    }
 
 ?>
 
