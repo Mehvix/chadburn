4b7088d29f5cec18c1cf081c330bde804e2afcc5
emly <emily2004.yao@gmail.com>
2020-07-18 21:21:00 -0500

    changing button look and adding images (v1.2)

--- index.php
+++ index.php
@@ -10,17 +10,17 @@
     <h1 class="header">Chadburn</h1>
 
     <div class="flex-container">
-        <div class="flex-child" style="background: #ffffab">
+        <div class="flex-child" style="background: #fae3d9">
             <form id="existing" action="viewer.php" method="POST">
                 <input class="menu" name="phrase" type="text" placeholder="Enter existing phrase">
-                <button class="menu hoverer" type="submit" style="background: #ffffab">=>
+                <button class="menu hoverer" type="submit" style="background: #ffffff">=>
                 </button>
             </form>
         </div>
 
-        <div class="flex-child" style="background: #f39292">
+        <div class="flex-child" style="background: #ffb6b9">
             <form id="new" action="create_chat.php" method="POST">
-                <input class="menu" name="new-phrase"" placeholder="Enter new phrase">
+                <input class="menu" name="new-phrase" placeholder="Enter new phrase">
                 <button class="menu hoverer" type="submit" style="background: #f39292">=>
                 </button>
             </form>
@@ -41,20 +41,24 @@
 
 <div class="explanation-left">
     <h3 class="explanation-left">Privacy First.</h3>
-    <p class="explanation-left">We don't store any identifiable information. The only thing needed to create a chat is a
-        meeting ID.</p>
+    <p class="explanation-left">We don't store any identifiable information. The<br> only thing needed to create a chat is a
+        meeting<br> ID.</p>
 </div>
+
 
 <div class="explanation-right">
     <h3 class="explanation-right">Security-oriented.</h3>
-    <p class="explanation-right">We've designed with security in mind, and the contents of each chat is hashed such that
-        even we can't read it.</p>
+    <p class="explanation-right">We've designed with security in mind, and the<br> contents of each chat is hashed such that
+        even<br> we can't read it.</p>
+    <img src="./image0.png" width="20%">
+
 </div>
 
 <div class="explanation-left">
     <h3 class="explanation-left">Free as in Speech.</h3>
     <p class="explanation-left">We have all of our code posted publicly so that you can verify it's legitimacy and host
         your own instance.</p>
+    <img src="./image1.png" width="20%">
 </div>
 
 </html>
