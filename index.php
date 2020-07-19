1d1f5f4243130bd35119d68d75163dfead78de18
MehvixDesktop <mehvix@protonmail.com>
2020-07-18 23:42:12 -0500

    favicon

--- index.php
+++ index.php
@@ -9,6 +9,18 @@
     <script type="text/javascript" src="jquery.youtube-background.js?v=1.0.7"></script>
     <script type="text/javascript" src="js/homepage.js"></script>
     <link rel='stylesheet' href='css/style.css'/>
+
+    <!-- Favicon-->
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/site.webmanifest">
+    <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="apple-mobile-web-app-title" content="Chadburn">
+    <meta name="application-name" content="Chadburn">
+    <meta name="msapplication-TileColor" content="#2d89ef">
+    <meta name="theme-color" content="#ffffff">
+
 </head>
 <body onload="getRandomChars()">
 <!-- Hero unit -->
