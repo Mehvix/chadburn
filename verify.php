59f0716733b6be50c67468486e3e4b3adfffcc37
MehvixDesktop <mehvix@protonmail.com>
2020-07-19 15:26:34 -0500

    cleaned up more stuff

--- verify.php
+++ verify.php
@@ -28,7 +28,6 @@ if (!$phrase) {
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
                 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-                <script type="text/javascript" src="js/jquery.youtube-background.js?v=1.0.7"></script>
                 <script src="js/viewer.js"></script>
                 <link rel='stylesheet' href='./css/verify.css'/>
 
