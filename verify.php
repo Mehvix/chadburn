3ba6b6cd17a6da2bddf7da0de08b266b354374fb
MehvixDesktop <mehvix@protonmail.com>
2020-07-19 04:00:19 -0500

    error message

--- verify.php
+++ verify.php
@@ -45,33 +45,33 @@ if (!$phrase) {
             </head>
             <body>
 
-            <div class="background" >
+            <div class="background">
 
-            <div class="middle" style="min-height: 100%">
+                <div class="middle" style="min-height: 100%">
 
-                <h2 align="center"><p>Chat: <code> <?php echo $phrase ?> </code></h2>
-                <br/>
-                <div class="container">
-                    <form method="POST" id="message_form">
-                        <div class="form-group">
-                            <input type="text" name="author" id="author" class="form-control"
-                                   placeholder="Enter Alias"/>
-                        </div>
-                        <div class="form-group">
+                    <h2 align="center"><p>Chat: <code> <?php echo $phrase ?> </code></h2>
+                    <br/>
+                    <div class="container">
+                        <form method="POST" id="message_form">
+                            <div class="form-group">
+                                <input type="text" name="author" id="author" class="form-control"
+                                       placeholder="Enter Alias"/>
+                            </div>
+                            <div class="form-group">
             <textarea name="message_content" id="message_content" class="form-control" placeholder="Enter Message"
                       rows="5"></textarea>
-                        </div>
-                        <div class="form-group">
-                            <input type="hidden" name="id" id="id" value="0"/>
-                            <input type="submit" name="submit" id="submit" class="btn btn-info"
-                                   value="Send Message"/>
-                        </div>
-                    </form>
-                    <span id="messages"></span>
-                    <br/>
-                    <div id="display_messages"></div>
+                            </div>
+                            <div class="form-group">
+                                <input type="hidden" name="id" id="id" value="0"/>
+                                <input type="submit" name="submit" id="submit" class="btn btn-info"
+                                       value="Send Message"/>
+                            </div>
+                        </form>
+                        <span id="messages"></span>
+                        <br/>
+                        <div id="display_messages"></div>
+                    </div>
                 </div>
-            </div>
 
             </body>
 
@@ -89,7 +89,7 @@ if (!$phrase) {
         }
 
     } catch (PDOException $e) {
-        die("<b>ERROR!</b> There is no chat that corresponds to the phrase <code>" . $phrase . "</code>...");
+        die("<b>ERROR!</b> There is no chat that corresponds to the phrase <code>" . $phrase . "</code>... If you're trying to access a chatroom with a long phrase/password, it can take a minute or so to generate ");
     }
 }
 
